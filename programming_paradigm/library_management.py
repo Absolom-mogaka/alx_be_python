36dad31483352df4614ae5b23c478341cb1dd29a
Medrin Kariuki <medrinkariuki@gmail.com>
2025-08-09 22:44:44 +0300

    Add Book and Library classes with required methods

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,6 +2,20 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+        self.is_checked_out = False
+
+    def check_out(self):
+        if not self.is_checked_out:
+            self.is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+            return True
+        return False
+
 
 class Library:
     def __init__(self):
@@ -9,3 +23,16 @@ class Library:
 
     def add_book(self, book):
         self.books.append(book)
+
+    def remove_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                return True
+        return False
+
+    def find_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                return book
+        return None
