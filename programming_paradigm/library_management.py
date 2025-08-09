32b1a38d055c501acd60d7fdb2099193d3aa4860
Medrin Kariuki <medrinkariuki@gmail.com>
2025-08-09 22:42:03 +0300

    Add Book and Library classes implementation

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,26 +1,11 @@
-# main-2.py
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
-from library_management import Book, Library
+class Library:
+    def __init__(self):
+        self.books = []
 
-def main():
-    # Setup a small library
-    library = Library()
-    library.add_book(Book("Brave New World", "Aldous Huxley"))
-    library.add_book(Book("1984", "George Orwell"))
-
-    # Initial list of available books
-    print("Available books after setup:")
-    library.list_available_books()
-
-    # Simulate checking out a book
-    library.check_out_book("1984")
-    print("\nAvailable books after checking out '1984':")
-    library.list_available_books()
-
-    # Simulate returning a book
-    library.return_book("1984")
-    print("\nAvailable books after returning '1984':")
-    library.list_available_books()
-
-if __name__ == "__main__":
-    main()
+    def add_book(self, book):
+        self.books.append(book)
